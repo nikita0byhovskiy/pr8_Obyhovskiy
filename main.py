a7e10485243d406a0f29d755d0e0bf6ba607d77b
unknown <nikitaobyhovskiy@yandex.ru>
2023-03-19 14:03:50 +0300

    Добавлен файл main.py цифры

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 def password():
-	token = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	token = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
 	result = ''
 	for i in range (11):
 		result = random.choice(token)
